44f8ec11e114b90310efb33fc3ddba30f93e1857
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2015-12-18 12:12:07 +0100

    use the parsley view in the RCP

--- parsley.example.rcp/src/parsley/example/rcp/Perspective.java
+++ parsley.example.rcp/src/parsley/example/rcp/Perspective.java
@@ -1,14 +1,18 @@
 package parsley.example.rcp;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
 		layout.setFixed(true);
-		
+
+		IFolderLayout folder = layout.createFolder("Table", IPageLayout.TOP, 0.25f, editorArea);
+		folder.addView("parsley.example.logeventstable");
 	}
 
 }
